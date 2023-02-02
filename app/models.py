d268b0d7dd9e8be57349a803b31cad9cc5650721
Joselo <jl.garciareymundo@gmail.com>
2023-02-02 01:58:45 -0600

    added db migration tool

--- app/models.py
+++ app/models.py
@@ -1,15 +1,14 @@
 from .database import Base
-from sqlalchemy import TIMESTAMP, Column, String, Integer, ForeignKey
+from sqlalchemy import TIMESTAMP, Column, String, Integer, ForeignKey, text
 from sqlalchemy.orm import relationship
-from sqlalchemy.schema import FetchedValue
 
 class Product(Base):
     __tablename__ = 'products'
     id = Column(Integer, primary_key=True, nullable=False)
     sku = Column(String, nullable=False, unique=True)
     inventories = relationship('Inventory', backref='product')
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=FetchedValue())
-    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_onupdate=FetchedValue())
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_onupdate=text("now()"))
 
 class Warehouse(Base):
     __tablename__ = 'warehouses'
@@ -17,8 +16,8 @@ class Warehouse(Base):
     sub_inventory = Column(String, nullable=False, unique=True)
     name = Column(String, nullable=False)
     inventories = relationship('Inventory', backref='warehouse')
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=FetchedValue())
-    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_onupdate=FetchedValue())
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_onupdate=text("now()"))
 
 class Inventory(Base):
     __tablename__ = 'inventories'
@@ -26,5 +25,5 @@ class Inventory(Base):
     warehouse_id = Column(Integer, ForeignKey('warehouses.id'), primary_key=True)
     product_id = Column(Integer, ForeignKey('products.id'), primary_key=True)
     quantity = Column(Integer)
-    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=FetchedValue())
-    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_onupdate=FetchedValue())
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+    updated_at = Column(TIMESTAMP(timezone=True), nullable=False, server_onupdate=text("now()"))
