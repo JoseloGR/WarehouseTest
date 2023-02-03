083f44177236ba3310a0e3ad4c1740757346e2ec
Joselo <jl.garciareymundo@gmail.com>
2023-02-03 13:27:43 -0600

    product schemas

---
+++ app/schemas.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from typing import List
+from pydantic import BaseModel
+
+class ProductBaseSchema(BaseModel):
+    id: int
+    sku: str
+
+    class Config:
+        orm_mode = True
+
+
+class CreateProductSchema(ProductBaseSchema):
+    pass
+
+
+class ProductResponse(ProductBaseSchema):
+    id: int
+    sku: str
+    created_at: datetime
+    updated_at: datetime
+
+
+class UpdateProductSchema(BaseModel):
+    id: int
+    sku: str
+    created_at: datetime | None
+    updated_at: datetime | None
+
+    class Config:
+        orm_mode = True
+
+
+class ListProductResponse(BaseModel):
+    status: str
+    results: int
+    products: List(ProductResponse)
+
+
+class WarehouseBaseSchema(BaseModel):
+    id: int
+    sub_inventory: str
+    name: str
+
