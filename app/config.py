8fd75850cad96679403698613b0b6344f832ed7a
Joselo <jl.garciareymundo@gmail.com>
2023-02-04 06:04:47 -0600

    config kafka env & setup producers and consumers

--- app/config.py
+++ app/config.py
@@ -9,6 +9,8 @@ class Settings(BaseSettings):
     POSTGRES_HOSTNAME: str
     CLIENT_ORIGIN: str
 
+    KAFKA_INSTANCE: str
+
     class Config:
         env_file = './.env'
 
--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
+import asyncio
+import json
+
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
 from app.routers import product, warehouse
+from app.schemas import ProducerMessage, ProducerResponse
 
 app = FastAPI()
 
@@ -20,6 +25,40 @@ app.add_middleware(
 
 app.include_router(product.router, tags=['Products'], prefix='/api/products')
 app.include_router(warehouse.router, tags=['Warehouses'], prefix='/api/warehouses')
+
+loop = asyncio.get_event_loop()
+aioproducer = AIOKafkaProducer(loop=loop, bootstrap_servers=settings.KAFKA_INSTANCE)
+consumer = AIOKafkaConsumer("orders", bootstrap_servers=settings.KAFKA_INSTANCE, loop=loop)
+
+
+async def consume():
+    await consumer.start()
+    try:
+        async for msg in consumer:
+            print("Consumed: ", msg.topic, msg.value)
+    finally:
+        await consumer.stop()
+
+
+@app.on_event("startup")
+async def startup_event():
+    await aioproducer.start()
+    loop.create_task(consume())
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await aioproducer.stop()
+    await consumer.stop()
+
+
+@app.post("/producer/{topicname}", tags=['Producers'])
+async def kafka_produce(msg: ProducerMessage, topicname: str):
+    await aioproducer.send(topicname, json.dumps(msg.dict()).encode("ascii"))
+    response = ProducerResponse(
+        name=msg.name, message_id=msg.message_id, topic=topicname
+    )
+    return response
 
 
 @app.get('/api/v1/healthcheck', tags=['Health'])
